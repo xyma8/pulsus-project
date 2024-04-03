036fcb6c390e99a3d6e6343de29ca7ecf07b1ac1
Nikita Semenov <bascetbol333@gmail.com>
2024-04-04 00:05:42 +0300

    Create pulsus

---
+++ backend/pulsus-backend/src/main/java/com/pulsus/pulsusbackend/PulsusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pulsus.pulsusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulsusBackendApplication.class, args);
+	}
+
+}
---
+++ backend/pulsus-backend/src/test/java/com/pulsus/pulsusbackend/PulsusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulsus.pulsusbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
